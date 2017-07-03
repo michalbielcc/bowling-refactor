8a9afabccebd5f3126a74779de6097f48c0cbca6
Bence Fabian <bence.fabian@codecool.com>
2017-07-03 11:07:06 +0200

    add get_value function
    * one branch with many `or`s instead `char in range(1,10)`
    * multiple branches with same return

--- bowling.py
+++ bowling.py
@@ -1,2 +1,16 @@
 def score(game):
     return 30
+
+def get_value(char):
+    if char == '1' or char == '2' or char == '3' or \
+       char == '4' or char == '5' or char == '6' or \
+       char == '7' or char == '8' or char == '9':
+        return int(char)
+    elif char == 'X':
+        return 10
+    elif char == '/':
+        return 10
+    elif char == '-':
+        return 0
+    else:
+        raise ValueError()
