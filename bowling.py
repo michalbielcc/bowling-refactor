6cc4b1144030f5b2a40d8b7c123015c7cc6c4895
Bence Fabian <bence.fabian@codecool.com>
2017-07-03 23:00:30 +0200

    every test passes
    major rewrites, but gotta be fine
    
    * == True
    * if/else instead of a = not a

--- bowling.py
+++ bowling.py
@@ -1,15 +1,31 @@
 def score(game):
     result = 0
-    left_of_frame = 0
+    frame = 1
+    in_first_half = True
     for i in range(len(game)):
-        result += get_value(game[i])
-        if get_value(game[i]) == 10:
+        if game[i] == '/':
+            result += 10 - last
+        else:
+            result += get_value(game[i])
+        if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
-                left_of_frame = 0
-                result -= last
+                result += get_value(game[i+1])
             elif game[i] == 'X':
-                left_of_frame = 0
+                result += get_value(game[i+1])
+                if game[i+2] == '/':
+                    result += 10 - get_value(game[i+1])
+                else:
+                    result += get_value(game[i+2])
         last = get_value(game[i])
+        if not in_first_half:
+            frame += 1
+        if in_first_half == True:
+            in_first_half = False
+        else:
+            in_first_half = True
+        if game[i] == 'X':
+            in_first_half = True
+            frame += 1
     return result
 
 def get_value(char):
