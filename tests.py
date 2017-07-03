e535b3d16cc1e101ffb029c7f5c1ffa98f1cd99b
Bence Fabian <bence.fabian@codecool.com>
2017-07-03 22:06:04 +0200

    added standard set of tests for kata

--- tests.py
+++ tests.py
@@ -2,6 +2,16 @@ import unittest
 from bowling import score
 
 class MyTests(unittest.TestCase):
+    def test_gutter_game(self):
+        self.assertEqual(score("--------------------"),  0)
+    def test_all_ones(self):
+        self.assertEqual(score("11111111111111111111"),  20)
+    def test_one_spare(self):
+        self.assertEqual(score("5/3-----------------"),  16)
+    def test_roll_strike(self):
+        self.assertEqual(score("X34----------------"),   24)
+    def test_perfect_game(self):
+        self.assertEqual(score("XXXXXXXXXXXX"),          300)
     def test_everything_is_fine_please(self):
         self.assertEqual(score("11111111112222222222"), 30)
         self.assertEqual(score("5/11------------3/11"), 26)
