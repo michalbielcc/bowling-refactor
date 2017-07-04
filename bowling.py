d8b397bca7ef0c8f2e3a09e22ba1bc972fbd0986
Bence Fabian <bence.fabian@codecool.com>
2017-07-04 09:07:49 +0200

    finally working

---
+++ bowling.py
@@ -0,0 +1,45 @@
+def score(game):
+    result = 0
+    frame = 1
+    in_first_half = True
+    for i in range(len(game)):
+        if game[i] == '/':
+            result += 10 - last
+        else:
+            result += get_value(game[i])
+        # if not in_first_half:
+            # frame += 1
+        if frame < 10  and get_value(game[i]) == 10:
+            if game[i] == '/':
+                result += get_value(game[i+1])
+            elif game[i] == 'X':
+                result += get_value(game[i+1])
+                if game[i+2] == '/':
+                    result += 10 - get_value(game[i+1])
+                else:
+                    result += get_value(game[i+2])
+        last = get_value(game[i])
+        if not in_first_half:
+            frame += 1
+        if in_first_half == True:
+            in_first_half = False
+        else:
+            in_first_half = True
+        if game[i] == 'X':
+            in_first_half = True
+            frame += 1
+    return result
+
+def get_value(char):
+    if char == '1' or char == '2' or char == '3' or \
+       char == '4' or char == '5' or char == '6' or \
+       char == '7' or char == '8' or char == '9':
+        return int(char)
+    elif char == 'X' or char == 'x':
+        return 10
+    elif char == '/':
+        return 10
+    elif char == '-':
+        return 0
+    else:
+        raise ValueError()
