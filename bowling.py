aa4ed593ffdaa8eacb9a0990f9b9bcfdeebcdde3
Michal Biel <mierwew@gmail.com>
2018-11-02 10:53:37 +0100

    function get_value simplified

--- bowling.py
+++ bowling.py
@@ -30,14 +30,14 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    try:
+        if int(char) in range(1,10):
+            return int(char)
+    except ValueError:
+        pass
+    if char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
